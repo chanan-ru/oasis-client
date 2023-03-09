4872ad89f8510a02c9c33797fd8a74fd1b1f945b
chanan-ru <chananchida.ru@gmail.com>
2023-03-09 22:18:23 +1100

    Integrated React Bootstrap

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,27 +1,40 @@
 import PropTypes from 'prop-types';
 
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from "react-bootstrap/Button";
+// import Badge from 'react-bootstrap/Badge';
+
+import './movie-view.scss';
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
-        <>
-            <div><img src={movie.image} alt={movie.title} /></div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.description}</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.genre}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.director}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
-        </>
+
+        <Row className="justify-content-md-center">
+            <Col md={5} lg={4} className="mb-3">
+                <div><img src={movie.image} alt={movie.title} /></div>
+            </Col>
+            <Col md={7} lg={6}>
+                <div>
+                    <span>Title: </span>
+                    <span>{movie.title}</span>
+                </div>
+                <div>
+                    <span>Description: </span>
+                    <span>{movie.description}</span>
+                </div>
+                <div>
+                    <span>Genre: </span>
+                    <span>{movie.genre}</span>
+                </div>
+                <div>
+                    <span>Director: </span>
+                    <span>{movie.director}</span>
+                </div>
+                <Button className="back-button mt-3" style={{ cursor: "pointer" }} onClick={onBackClick}>Back</Button>
+            </Col>
+
+        </Row>
     );
 };
 
